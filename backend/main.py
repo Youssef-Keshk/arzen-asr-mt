183922a4ba12766d993744bd2ae23a031ecd3965
Youssef-Keshk <youssefkeshk001@gmail.com>
2026-05-13 03:20:27 +0300

    Add auto models loading on server startup

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,38 @@
 ASR & MT Backend — FastAPI + Uvicorn  v4.0
 """
 
+from contextlib import asynccontextmanager
+import asyncio
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api import router
+from api import router, warmup, health
 
-app = FastAPI(title="ASR & MT API", version="4.0.0")
+async def wait_for_models_and_print_health():
+    """Background task that polls health until models are loaded."""
+    
+    while True:
+        current_health = await health()
+        if current_health["asr_loaded"] and current_health["mt_loaded"]:
+            print("\n" + "="*40)
+            print(current_health)
+            print("="*40 + "\n")
+            break
+
+        await asyncio.sleep(3)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("🚀 Server starting: Invoking warmup...")
+
+    await warmup()
+    
+    asyncio.create_task(wait_for_models_and_print_health())
+    
+    yield
+    
+    print("🛑 Server shutting down...")
+
+app = FastAPI(title="ASR & MT API", version="4.0.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,5 +42,5 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Include all the endpoints defined in api.py
+
 app.include_router(router)
